1a481c0f92f97929b623720707d1d2964e53905a
Ayo-Show <sowunmi30@gmail.com>
2022-02-22 13:48:03 -0500

    Added play and pause functionality

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const video = document.getElementById("video");
 const videoControls = document.getElementById("video-controls");
+const playButton = document.getElementById("play");
+
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 const videoWorks = !!document.createElement("video").canPlayType;
 if (videoWorks) {
   videoControls = false;
   videoControls.classList.remove("hidden");
 }
+
+playButton.addEventListener("click", togglePlay);
