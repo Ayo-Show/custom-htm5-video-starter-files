d1fc670a174220d77a2425392c662ed4d211a3df
Ayo-Show <sowunmi30@gmail.com>
2022-02-22 18:21:37 -0500

    Added function to show the video duration and time elapsed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const video = document.getElementById("video");
 const videoControls = document.getElementById("video-controls");
 const playButton = document.getElementById("play");
 const playbackIcons = document.querySelectorAll(".playback-icons use");
+const timeElapsed = document.getElementById("time-elapsed");
+const duration = document.getElementById("duration");
 
 const videoWorks = !!document.createElement("video").canPlayType;
 if (videoWorks) {
@@ -29,6 +31,30 @@ function updatePlayButton() {
   }
 }
 
+function formatTime(timeInSeconds) {
+  const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
+}
+
+function initializeVideo() {
+  const videoDuration = Math.round(video.duration);
+  const time = formatTime(videoDuration);
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute("datetime", `${time.minutes}m ${time.seconds}s`);
+}
+
+function updateTimeElapsed() {
+  const time = formatTime(Math.round(video.currentTime));
+  timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  timeElapsed.setAttribute("datetime", `${time.minutes}m ${time.seconds}s`);
+}
+
 playButton.addEventListener("click", togglePlay);
 video.addEventListener("pause", updatePlayButton);
 video.addEventListener("play", updatePlayButton);
+video.addEventListener("loadedmetadata", initializeVideo);
+video.addEventListener("timeupdate", updateTimeElapsed);
