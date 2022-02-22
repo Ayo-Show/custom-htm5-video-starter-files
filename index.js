9655e387029124ccb48462350711a171db48bd82
Ayo-Show <sowunmi30@gmail.com>
2022-02-22 14:13:21 -0500

    updated function to change text in tooltip

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const video = document.getElementById("video");
 const videoControls = document.getElementById("video-controls");
 const playButton = document.getElementById("play");
+const playbackIcons = document.querySelectorAll(".playback-icons use");
+
+const videoWorks = !!document.createElement("video").canPlayType;
+if (videoWorks) {
+  video.controls = false;
+  videoControls.classList.remove("hidden");
+}
 
 function togglePlay() {
   if (video.paused || video.ended) {
@@ -10,10 +17,18 @@ function togglePlay() {
   }
 }
 
-const videoWorks = !!document.createElement("video").canPlayType;
-if (videoWorks) {
-  videoControls = false;
-  videoControls.classList.remove("hidden");
+// updatePlayButton updates the playback icon and tooltip
+// depending on the playback state
+function updatePlayButton() {
+  playbackIcons.forEach((icon) => icon.classList.toggle("hidden"));
+
+  if (video.paused) {
+    playButton.setAttribute("data-title", "Play (k)");
+  } else {
+    playButton.setAttribute("data-title", "Pause (k)");
+  }
 }
 
 playButton.addEventListener("click", togglePlay);
+video.addEventListener("pause", updatePlayButton);
+video.addEventListener("play", updatePlayButton);
