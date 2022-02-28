06dab712c4d1c98d21260fe1dde6fb51d6e87982
Ayo-Show <sowunmi30@gmail.com>
2022-02-28 12:52:39 -0500

    added function for volume controls

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const duration = document.getElementById("duration");
 const progressBar = document.getElementById("progress-bar");
 const seek = document.getElementById("seek");
 const seekTooltip = document.getElementById("seek-tooltip");
+const volumeButton = document.getElementById("volume-button");
+const volumeIcons = document.querySelectorAll(".volume-button use");
+const volumeMute = document.querySelector('use[href="#volume-mute"]');
+const volumeLow = document.querySelector('use[href="#volume-low"]');
+const volumeHigh = document.querySelector('use[href="#volume-high"]');
+const volume = document.getElementById("volume");
 
 const videoWorks = !!document.createElement("video").canPlayType;
 if (videoWorks) {
@@ -45,6 +51,32 @@ function updateSeekTooltip(event) {
   const rect = video.getBoundingClientRect();
   seekTooltip.style.left = `${event.pageX - rect.left}px`;
 }
+
+function updateVolume() {
+  if (video.muted) {
+    video.muted = false;
+  }
+
+  video.volume = volume.value;
+}
+
+function updateVolumeIcon() {
+  volumeIcons.forEach((icon) => {
+    icon.classList.add("hidden");
+  });
+
+  volumeButton.setAttribute("data-title", "Mute (m)");
+
+  if (video.muted || video.volume === 0) {
+    volumeMute.classList.remove("hidden");
+    volumeButton.setAttribute("data-title", "Unmute (m)");
+  } else if (video.volume > 0 && video.volume <= 0.5) {
+    volumeLow.classList.remove("hidden");
+  } else {
+    volumeHigh.classList.remove("hidden");
+  }
+}
+
 
 function formatTime(timeInSeconds) {
   const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
@@ -94,3 +126,6 @@ video.addEventListener("timeupdate", updateTimeElapsed);
 video.addEventListener("timeupdate", updateProgress);
 seek.addEventListener("mousemove", updateSeekTooltip);
 seek.addEventListener("input", skipAhead);
+volume.addEventListener("input", updateVolume);
+video.addEventListener("volumechange", updateVolumeIcon);
+
