41390d183a7121f41a9d2021848b2757ec0418c6
Ayo-Show <sowunmi30@gmail.com>
2022-02-28 14:11:51 -0500

    Added picture-in-picture support

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const playbackAnimation = document.getElementById("playback-animation");
 const fullscreenButton = document.getElementById("fullscreen-button");
 const videoContainer = document.getElementById("video-container");
 const fullscreenIcons = fullscreenButton.querySelectorAll("use");
+const pipButton = document.getElementById("pip-button");
 
 const videoWorks = !!document.createElement("video").canPlayType;
 if (videoWorks) {
@@ -132,6 +133,22 @@ function skipAhead(event) {
   seek.value = skipTo;
 }
 
+// togglePip toggles Picture-in-Picture mode on the video
+async function togglePip() {
+  try {
+    if (video !== document.pictureInPictureElement) {
+      pipButton.disabled = true;
+      await video.requestPictureInPicture();
+    } else {
+      await document.exitPictureInPicture();
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pipButton.disabled = false;
+  }
+}
+
 // toggleFullScreen toggles the full screen state of the video
 // If the browser is currently in fullscreen mode,
 // then it should exit and vice versa.
@@ -194,5 +211,11 @@ video.addEventListener("volumechange", updateVolumeIcon);
 volumeButton.addEventListener("click", toggleMute);
 video.addEventListener("click", togglePlay);
 video.addEventListener("click", animatePlayback);
-videoContainer.addEventListener('fullscreenchange', updateFullscreenButton);
-fullscreenButton.addEventListener('click', toggleFullScreen);
+videoContainer.addEventListener("fullscreenchange", updateFullscreenButton);
+fullscreenButton.addEventListener("click", toggleFullScreen);
+document.addEventListener("DOMContentLoaded", () => {
+  if (!("pictureInPictureEnabled" in document)) {
+    pipButton.classList.add("hidden");
+  }
+});
+pipButton.addEventListener("click", togglePip);
